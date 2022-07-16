033c002493ecbcc58fd705d261107a59ec8bc410
Alexey Maslov <alvladigital@gmail.com>
2022-07-16 14:31:36 +0300

    add task13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 13: Напишите программу, которая с помощью деления выводит 
+третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Insert number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int modulnumber = Math.Abs(number);
+if (modulnumber < 100)
+{
+    Console.WriteLine("Third digital is absent");
+}
+
+else
+{
+while (modulnumber > 999)
+{
+    modulnumber = modulnumber / 10;
+    Console.WriteLine($"{modulnumber}");
+}
+int thirddigital = 0;
+thirddigital += modulnumber % 10;
+Console.WriteLine($"Thrid digital is {thirddigital}");
+}
